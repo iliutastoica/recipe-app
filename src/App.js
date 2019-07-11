8029b754d310bfbaf08bcd4e3711bd469dc30612
Ili Stoica <iliuta.stoica@gmail.com>
2019-07-11 12:03:52 +0300

    adding files

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const config = require('./config/site');
+
+const API_KEY = config.APP_KEY; // 'd7a8cdde9f02265ddde5d843d6f65914'; //
+// const API_KEY2 = config.APP_KEY2;
+console.log(API_KEY);
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`).catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ', error.message);
+    });
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  }
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes });
+  }
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+  render() {
+    const recipes = this.state.recipes;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={recipes} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/config/site.js
@@ -0,0 +1,20 @@
+module.exports = {
+    pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+    title: 'Recipe APP', // Navigation and Site Title
+    titleAlt: 'Recipe APP', // Title for JSONLD
+    description: 'Food Recipe APP',
+    url: 'https://ilistoica.dev', // Domain of your site. No trailing slash!
+    siteUrl: 'https://ilistoica.dev', // url + pathPrefix
+    siteLanguage: 'en', // Language Tag on <html> element
+    logo: 'static/logo/logo.png', // Used for SEO
+    banner: 'static/logo/banner.png',
+    // JSONLD / Manifest
+    favicon: 'icons/icon-192x192.png', // Used for manifest favicon generation
+    shortName: 'RecipeAPP', // shortname for manifest. MUST be shorter than 12 characters
+    author: '@iliutastoica', // Author for schemaORGJSONLD
+    themeColor: '#2772d0',
+    backgroundColor: '#2772d0',
+    twitter: '@iliutastoica', // Twitter Username
+    APP_KEY: 'd7a8cdde9f02265ddde5d843d6f65914',
+    APP_KEY2: 'aedbb2d845263a9cad4857bcec585195',
+  };
